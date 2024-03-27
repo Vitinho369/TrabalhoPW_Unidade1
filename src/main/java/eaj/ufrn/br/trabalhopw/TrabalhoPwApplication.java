2c86e67017ff57afae11ec619af8823f2c32e1d4
Vitinho369 <vitinho21.2005@gmail.com>
2024-03-27 19:48:05 -0300

    Initial commit

---
+++ src/main/java/eaj/ufrn/br/trabalhopw/TrabalhoPwApplication.java
@@ -0,0 +1,13 @@
+package eaj.ufrn.br.trabalhopw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrabalhoPwApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrabalhoPwApplication.class, args);
+	}
+
+}
---
+++ src/test/java/eaj/ufrn/br/trabalhopw/TrabalhoPwApplicationTests.java
@@ -0,0 +1,13 @@
+package eaj.ufrn.br.trabalhopw;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrabalhoPwApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
